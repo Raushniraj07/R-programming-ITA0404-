4f3fbd2cb4a800ef3ebe60c86ca38ed01e99d9b8
Raushni Raj <112066251+Raushniraj07@users.noreply.github.com>
2023-08-25 13:41:02 +0530

    Update ex-8 Lower& Upper case.R

--- ex-8 Lower& Upper case.R
+++ ex-8 Lower& Upper case.R
@@ -7,3 +7,11 @@ print(t)
 print("Letters between 22nd to 24th letters in upper case:")
 e = tail(LETTERS[22:24])
 print(e)
+
+output:-
+[1] "First 10 letters in lower case:"
+ [1] "a" "b" "c" "d" "e" "f" "g" "h" "i" "j"
+[1] "Last 10 letters in upper case:"
+ [1] "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z"
+[1] "Letters between 22nd to 24th letters in upper case:"
+[1] "V" "W" "X"
