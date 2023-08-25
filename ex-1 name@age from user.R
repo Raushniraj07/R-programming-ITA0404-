0b17151f35cb50a2ca32f702a624f0e1dfb3f874
Raushni Raj <112066251+Raushniraj07@users.noreply.github.com>
2023-08-25 12:44:35 +0530

    Update ex-1 name@age from user.R

--- ex-1 name@age from user.R
+++ ex-1 name@age from user.R
@@ -1,3 +1,8 @@
 name<-readline("enter Your name:")
 age<-as.numeric(readline("enter your age:"))
 cat("your name is:",name,"and your age is:",age,"\n")
+
+output:-
+enter Your name:Raush
+enter your age:20
+your name is: Raush and your age is: 20 
