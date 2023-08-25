fcebd88f5246f46140eb713e4220c8af02ae51dd
Raushni Raj <112066251+Raushniraj07@users.noreply.github.com>
2023-08-25 13:48:56 +0530

    Update ex-9 factor of given num.R

--- ex-9 factor of given num.R
+++ ex-9 factor of given num.R
@@ -9,3 +9,19 @@ print_factors = function(n) {
 print_factors(4)
 print_factors(7)
 print_factors(12)
+
+Input:-
+[1] "The factors of 4 are:"
+[1] 1
+[1] 2
+[1] 4
+[1] "The factors of 7 are:"
+[1] 1
+[1] 7
+[1] "The factors of 12 are:"
+[1] 1
+[1] 2
+[1] 3
+[1] 4
+[1] 6
+[1] 12
