7cf1abc4db77779a34025218cb295dfb89980a6b
Raushni Raj <112066251+Raushniraj07@users.noreply.github.com>
2023-08-25 14:20:29 +0530

    Update ex-15 vector and datatype.R

--- ex-15 vector and datatype.R
+++ ex-15 vector and datatype.R
@@ -7,3 +7,11 @@ print(b)
 print(typeof(b))
 print(c)
 print(typeof(c))
+
+output:-
+[1]  1  2  5  3  4  0 -1 -3
+[1] "double"
+[1] "Red"   "Green" "White"
+[1] "character"
+[1]  TRUE  TRUE  TRUE FALSE  TRUE FALSE
+[1] "logical"
