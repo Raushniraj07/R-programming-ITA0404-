ab8a9390470d0dd9721023f264ed5719d8b0f23e
Raushni Raj <112066251+Raushniraj07@users.noreply.github.com>
2023-08-25 12:49:07 +0530

    Update ex-3 seq,mean,sum.R

--- ex-3 seq,mean,sum.R
+++ ex-3 seq,mean,sum.R
@@ -1,12 +1,15 @@
-#print("sequence of number from 20 to 50:")
-#print(seq(20,50))
-#print("mean of number from 20 to 60:")
-#print(mean(20:60))
-#print("sum of num from 51 to 91:")
-#print(sum(51:91))
-sequence_20_to_50<-seq(20,50)
-mean_20_to_50<-mean(seq(20,60))
-sum_51_to_91<-sum(seq(51,91))
-sequence_20_to_50
-mean_20_to_50
-sum_51_to_91
+print("sequence of number from 20 to 50:")
+print(seq(20,50))
+print("mean of number from 20 to 60:")
+print(mean(20:60))
+print("sum of num from 51 to 91:")
+print(sum(51:91))
+
+output:-
+[1] "sequence of number from 20 to 50:"
+ [1] 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47
+[29] 48 49 50
+[1] "mean of number from 20 to 60:"
+[1] 40
+[1] "sum of num from 51 to 91:"
+[1] 2911
