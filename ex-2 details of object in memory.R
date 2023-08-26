15d18e7b38961012abb69e77beb108a55bf332ae
Raushni Raj <112066251+Raushniraj07@users.noreply.github.com>
2023-08-26 09:24:14 +0530

    Update ex-2 details of object in memory.R

--- ex-2 details of object in memory.R
+++ ex-2 details of object in memory.R
@@ -5,3 +5,16 @@ nums = c(10, 20, 30, 40, 50, 60)
 print(ls())
 print("Details of the objects in memory:")
 print(ls.str())
+
+input:-
+[1] "df"   "n1"   "n2"   "name" "nums"
+[1] "Details of the objects in memory:"
+df : 'data.frame':	4 obs. of  4 variables:
+ $ n   : num  1 1 2 2
+ $ time: num  1 2 1 2
+ $ x   : num  6 3 2 5
+ $ y   : num  1 4 6 9
+n1 :  num 10
+n2 :  num 0.5
+name :  chr "Python"
+nums :  num [1:6] 10 20 30 40 50 60
